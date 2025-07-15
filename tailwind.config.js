0a5f476f68cf0bc80dab7dbd6c7e67280ff0a720
dhopegraphics <dhopegraphics@gmail.com>
2025-07-15 23:17:57 +0000

    chore: update Tailwind CSS configuration and TypeScript paths, remove unused font file, add placeholder files for community and profile pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./constants/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,jsx,ts,tsx}",
+    "./context/**/*.{js,jsx,ts,tsx}",
+    "./utils/**/*.{js,jsx,ts,tsx}",
+    "./backend/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDark: "#03624C", // Bangladesh Green
+        primaryLight: "#2CC295", // Meadow
+        dark: "#042222", // Rich Black
+        darkLight: "#093535", // Rich Black
+        light: "#F8FBFF", // Light Blue
+        deepLight: "#DDE6F0",
+        offwhite: "#FFE4D0", // Snow
+        lightGray: "#E2E8F0",
+        white: "#FFFFFF",
+        black: "#000000",
+        error: "#FF5252",
+        gray: "#94A3B8",
+      },
+    },
   },
+
   plugins: [],
 };
