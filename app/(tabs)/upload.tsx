db8485afa07337ac42b74ecc97def0b056bc8280
dhopegraphics <dhopegraphics@gmail.com>
2025-07-16 17:10:16 +0000

    feat: add initial tab components for discover, index, planner, profile, and upload

---
+++ app/(tabs)/upload.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const upload = () => {
+  return (
+    <View>
+      <Text>upload</Text>
+    </View>
+  );
+};
+
+export default upload;
