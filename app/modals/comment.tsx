a14adcc535e931b109388fc7d0c458ed59e30d13
dhopegraphics <dhopegraphics@gmail.com>
2025-07-17 05:31:05 +0000

    feat: add new components for community, modals, onboarding, post, profile, and search

---
+++ app/modals/comment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const comment = () => {
+  return (
+    <View>
+      <Text>comment</Text>
+    </View>
+  );
+};
+
+export default comment;
